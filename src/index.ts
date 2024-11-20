5d6b24232489ca3f7b0887c8f2c712e08ae32e48
Brihat576 <thapabrihatbir321@gmail.com>
2024-11-20 21:39:19 +0530

    added the content route

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,32 @@ import express from "express"
 const app=express()
 import jwt from "jsonwebtoken"
 const JWT_SECRET='ilovekiara'
-import { userModel } from "./db"
+import { userModel,contentModel } from "./db"
+import  {Middleware}  from "./middleware"
+import {z} from "zod"
+import mongoose from "mongoose"
 app.use(express.json())
 
+async function Main(){
+    await mongoose.connect("mongodb+srv://brihat:Brihatbir1@cluster0.ymp18.mongodb.net/brainly")
+    console.log("connected to the database")
+    app.listen(3000,()=>{
+        console.log("listening on port 3000")
+    })
+}
+Main()
+
 app.post("/api/v1/signup",async(req,res)=>{
+    const requiredBody=z.object({
+        username:z.string().min(3).max(10),
+        password:z.string().min(3).max(12)
+    })
+    const parsedData=requiredBody.safeParse(req.body)
+    if(!parsedData.success){
+        res.json({
+            message:parsedData.error.message
+        })
+    }
     const{username,password}=req.body
     await userModel.create({
         username:username,
@@ -40,13 +62,28 @@ app.post("/api/v1/login",async(req,res)=>{
 
 
 })
-app.post("/api/v1/brain/share",(req,res)=>{
 
-})
-app.post("/api/v1/content",(rqe,res)=>{
+app.post("/api/v1/content",Middleware,async(req,res)=>{
+    const{link,title}=req.body
+    await contentModel.create({
+        link,
+        title,
+        //@ts-ignore
+        userId:req.userId,
+        tags:[]
+    })
+    res.json({
+        message:"your content has been updated successfully"
+    })
     
 
 })
+app.put("/api/v1/content/:id",Middleware,(req,res)=>{
+
+})
 app.get("/api/v1/content",(req,res)=>{
+
+})
+app.post("/api/v1/brain/share",(req,res)=>{
 
 })
